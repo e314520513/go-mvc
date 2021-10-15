2f4227b4edb72ad161425b19617bbebe0a966367
wahaha <e314520513@gmail.com>
2021-10-15 14:51:38 +0800

    update

--- routers/router.go
+++ routers/router.go
@@ -7,7 +7,7 @@ import(
 func Router(){
 
 	http.HandleFunc("/",controllers.Home)
-
+	log.Println("Listening on localhost:9090")
 	if err:= http.ListenAndServe(":9090",nil); err!=nil{
 		log.Fatal("ListenAndServe: ",err)
 	}
