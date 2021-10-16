7c7c49fb05833b093054f638cebb4a387a4b60de
wahaha <e314520513@gmail.com>
2021-10-16 20:43:31 +0800

    update

--- src/connection/connection.go
+++ src/connection/connection.go
@@ -17,7 +17,14 @@ const(
 	MaxOpenConns  int    = 10
 	MaxIdleConns  int    = 10
 )
-
+type User struct {
+	ID        int64     `gorm:"type:bigint(20) NOT NULL auto_increment;primary_key;" json:"id,omitempty"`
+	Username  string    `gorm:"type:varchar(20) NOT NULL;" json:"username,omitempty"`
+	Password  string    `gorm:"type:varchar(100) NOT NULL;" json:"password,omitempty"`
+	Status    int32     `gorm:"type:int(5);" json:"status,omitempty"`
+	CreatedAt time.Time `gorm:"type:timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP" json:"created_at,omitempty"`
+	UpdatedAt time.Time `gorm:"type:timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP" json:"updated_at,omitempty"`
+}
 
 func ConnectDatabase(){
 	//組合sql連線字串
@@ -38,6 +45,16 @@ func ConnectDatabase(){
 	}
 	db.SetConnMaxLifetime(time.Duration(MaxLifetime) * time.Second)
 	db.SetMaxIdleConns(MaxIdleConns)
-	db.SetMaxOpenConns(MaxOpenConns)	
+	db.SetMaxOpenConns(MaxOpenConns)
+	
+	//產生table
+	conn.Debug().AutoMigrate(&User{})
+	//判斷有沒有table存在
+	migrator := conn.Migrator()
+	has := migrator.HasTable(&User{})
+	//has := migrator.HasTable("GG")
+	if !has {
+		fmt.Println("table not exist")
+	}
 	
 }
