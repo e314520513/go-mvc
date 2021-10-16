b040abbe06393f28705e866bb42644039c567dab
wahaha <e314520513@gmail.com>
2021-10-16 11:46:51 +0800

    update config fo database connection

--- src/connection/connection.go
+++ src/connection/connection.go
@@ -1,21 +1,36 @@
 package connection
 
-import(
+import ( 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
+const(
+	UserName      string = "golang_user"
+	Password      string = "golang_passw0rd"
+	Addr          string = "127.0.0.1"
+	Port      	  int    = 4306	   
+	Database      string = "godb"
+	MaxLifetime   int    = 10
+	MaxOpenConns  int    = 10
+	MaxIdleConns  int    = 10
+)
+
 
 func ConnectDatabase(){
-	dsn := "homestead:secret@tcp(127.0.0.1:4306)/homestead?charset=utf8&parseTime=True&loc=Local"
-	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	//組合sql連線字串
+	addr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True",UserName,Password,Addr,Port,Database)
+	//連接mysql
+	database, err := gorm.Open(mysql.Open(addr), &gorm.Config{})
 
 	if err != nil {
-		panic("Failed to connect to database!")
+		fmt.Println("get db failed:",err)
 	}
+	db.SetConnMaxLifetime(time.Duration(MaxLifetime) * time.Second)
+	db.SetMaxIdleConns(MaxIdleConns)
+	db.SetMaxOpenConns(MaxOpenConns)
+
+
 
 	
-
-	DB = database
 }
